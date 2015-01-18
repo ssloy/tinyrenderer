d9c4b14c0d8c385937bc87cee1178f1e42966b7c
ssloy <spam@skisa.org>
2015-01-18 18:05:49 +0100

    ybuffer!

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <vector>
 #include <cmath>
+#include <limits>
 #include "tgaimage.h"
-#include "model.h"
 #include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
-Model *model = NULL;
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
 const int width  = 800;
-const int height = 800;
+const int height = 500;
 
 void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -24,7 +24,7 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
 
     for (int x=p0.x; x<=p1.x; x++) {
         float t = (x-p0.x)/(float)(p1.x-p0.x);
-        int y = p0.y*(1.-t) + p1.y*t;
+        int y = p0.y*(1.-t) + p1.y*t + .5;
         if (steep) {
             image.set(y, x, color);
         } else {
@@ -33,55 +33,55 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     }
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
-    if (t0.y>t1.y) std::swap(t0, t1);
-    if (t0.y>t2.y) std::swap(t0, t2);
-    if (t1.y>t2.y) std::swap(t1, t2);
-    int total_height = t2.y-t0.y;
-    for (int i=0; i<total_height; i++) {
-        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
-        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
-        float alpha = (float)i/total_height;
-        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
-        Vec2i A =               t0 + (t2-t0)*alpha;
-        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
-        if (A.x>B.x) std::swap(A, B);
-        for (int j=A.x; j<=B.x; j++) {
-            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[]) {
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
+    }
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t + .5;
+        if (ybuffer[x]<y) {
+            ybuffer[x] = y;
+            image.set(x, 0, color);
         }
     }
 }
 
 int main(int argc, char** argv) {
-    if (2==argc) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("obj/african_head.obj");
+    { // just dumping the 2d scene (yay we have enough dimensions!)
+        TGAImage scene(width, height, TGAImage::RGB);
+
+        // scene "2d mesh"
+        line(Vec2i(20, 34),   Vec2i(744, 400), scene, red);
+        line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
+        line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
+
+        // screen line
+        line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
+
+        scene.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        scene.write_tga_file("scene.tga");
     }
 
-    TGAImage image(width, height, TGAImage::RGB);
-    Vec3f light_dir(0,0,-1);
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j=0; j<3; j++) {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
-            world_coords[j]  = v;
+    {
+        TGAImage render(width, 16, TGAImage::RGB);
+        int ybuffer[width];
+        for (int i=0; i<width; i++) {
+            ybuffer[i] = std::numeric_limits<int>::min();
         }
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
-        n.normalize();
-        float intensity = n*light_dir;
-        if (intensity>0) {
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
-        }
-    }
+        rasterize(Vec2i(20, 34),   Vec2i(744, 400), render, red,   ybuffer);
+        rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
+        rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue,  ybuffer);
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    delete model;
+        // 1-pixel wide image is bad for eyes, lets widen it
+        for (int i=0; i<width; i++) {
+            for (int j=1; j<16; j++) {
+                render.set(i, j, render.get(i, 0));
+            }
+        }
+        render.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        render.write_tga_file("render.tga");
+    }
     return 0;
 }
 
