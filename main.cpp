7e46cc57fa3f5a41129d6b6fefe4e77f77b8aa84
ssloy <spam@skisa.org>
2015-01-18 08:50:49 +0100

    better test triangles

--- main.cpp
+++ main.cpp
@@ -40,9 +40,9 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
 
-    Vec2i t0[3] = {Vec2i(10, 10),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
     Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 150), Vec2i(130, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
 
     triangle(t0[0], t0[1], t0[2], image, red);
     triangle(t1[0], t1[1], t1[2], image, white);
