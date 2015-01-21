0943d1ea82f32f09a75a0b6d4d10d7a2732c370c
ssloy <ssloy@castille.loria.fr>
2015-01-21 10:51:42 +0100

    got rid of unions (ub)

--- geometry.h
+++ geometry.h
@@ -3,38 +3,52 @@
 
 #include <cmath>
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 template <class t> struct Vec2 {
-	union {
-		struct {t u, v;};
-		struct {t x, y;};
-		t raw[2];
-	};
-	Vec2() : u(0), v(0) {}
-	Vec2(t _u, t _v) : u(_u),v(_v) {}
-	inline Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(u+V.u, v+V.v); }
-	inline Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(u-V.u, v-V.v); }
-	inline Vec2<t> operator *(float f)          const { return Vec2<t>(u*f, v*f); }
-	template <class > friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
+    t raw[2];
+    t& x;
+    t& y;
+    Vec2<t>() : raw(), x(raw[0]), y(raw[1]) { x = y = t(); }
+    Vec2<t>(t _x, t _y) : raw(), x(raw[0]), y(raw[1]) { x=_x; y=_y; }
+    Vec2<t>(const Vec2<t> &v) : raw(), x(raw[0]), y(raw[1]) { *this = v; }
+    Vec2<t> & operator =(const Vec2<t> &v) {
+        if (this != &v) {
+            x = v.x;
+            y = v.y;
+        }
+        return *this;
+    }
+    Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(x+V.x, y+V.y); }
+    Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(x-V.x, y-V.y); }
+    Vec2<t> operator *(float f)          const { return Vec2<t>(x*f, y*f); }
+    t& operator[](const int i) {return raw[i];}
+    template <class > friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
 };
 
 template <class t> struct Vec3 {
-	union {
-		struct {t x, y, z;};
-		struct { t ivert, iuv, inorm; };
-		t raw[3];
-	};
-	Vec3() : x(0), y(0), z(0) {}
-	Vec3(t _x, t _y, t _z) : x(_x),y(_y),z(_z) {}
-	inline Vec3<t> operator ^(const Vec3<t> &v) const { return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); }
-	inline Vec3<t> operator +(const Vec3<t> &v) const { return Vec3<t>(x+v.x, y+v.y, z+v.z); }
-	inline Vec3<t> operator -(const Vec3<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
-	inline Vec3<t> operator *(float f)          const { return Vec3<t>(x*f, y*f, z*f); }
-	inline t       operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
-	float norm () const { return std::sqrt(x*x+y*y+z*z); }
-	Vec3<t> & normalize(t l=1) { *this = (*this)*(l/norm()); return *this; }
-	template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
+    t raw[3];
+    t& x;
+    t& y;
+    t& z;
+    Vec3<t>() : raw(), x(raw[0]), y(raw[1]), z(raw[2]) { x = y = z = t(); }
+    Vec3<t>(t _x, t _y, t _z) : raw(), x(raw[0]), y(raw[1]), z(raw[2]) { x=_x; y=_y; z=_z; }
+    Vec3<t>(const Vec3<t> &v) : raw(), x(raw[0]), y(raw[1]), z(raw[2]) { *this = v; }
+    Vec3<t> & operator =(const Vec3<t> &v) {
+        if (this != &v) {
+            x = v.x;
+            y = v.y;
+            z = v.z;
+        }
+        return *this;
+    }
+    Vec3<t> operator ^(const Vec3<t> &v) const { return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); }
+    Vec3<t> operator +(const Vec3<t> &v) const { return Vec3<t>(x+v.x, y+v.y, z+v.z); }
+    Vec3<t> operator -(const Vec3<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
+    Vec3<t> operator *(float f)          const { return Vec3<t>(x*f, y*f, z*f); }
+    t       operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
+    float norm () const { return std::sqrt(x*x+y*y+z*z); }
+    Vec3<t> & normalize(t l=1) { *this = (*this)*(l/norm()); return *this; }
+    t& operator[](const int i) {return raw[i];}
+    template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
 };
 
 typedef Vec2<float> Vec2f;
@@ -43,13 +57,13 @@ typedef Vec3<float> Vec3f;
 typedef Vec3<int>   Vec3i;
 
 template <class t> std::ostream& operator<<(std::ostream& s, Vec2<t>& v) {
-	s << "(" << v.x << ", " << v.y << ")\n";
-	return s;
+    s << "(" << v.x << ", " << v.y << ")\n";
+    return s;
 }
 
 template <class t> std::ostream& operator<<(std::ostream& s, Vec3<t>& v) {
-	s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n";
-	return s;
+    s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n";
+    return s;
 }
 
 #endif //__GEOMETRY_H__
--- model.cpp
+++ model.cpp
@@ -17,7 +17,7 @@ Model::Model(const char *filename) : verts_(), faces_() {
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
             Vec3f v;
-            for (int i=0;i<3;i++) iss >> v.raw[i];
+            for (int i=0;i<3;i++) iss >> v[i];
             verts_.push_back(v);
         } else if (!line.compare(0, 2, "f ")) {
             std::vector<int> f;
