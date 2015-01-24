ed874da66cdd8250422980770555d01cc6222f70
ssloy <spam@skisa.org>
2015-01-24 15:19:19 +0100

    lookat()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 #include <cmath>
 #include <limits>
 #include "tgaimage.h"
@@ -11,8 +12,9 @@ const int depth  = 255;
 
 Model *model = NULL;
 int *zbuffer = NULL;
-Vec3f light_dir = Vec3f(1,1,1).normalize();
-Vec3f camera(0,0,5);
+Vec3f light_dir = Vec3f(1,-1,1).normalize();
+Vec3f eye(1,1,3);
+Vec3f center(0,0,0);
 
 Matrix viewport(int x, int y, int w, int h) {
     Matrix m = Matrix::identity(4);
@@ -24,6 +26,20 @@ Matrix viewport(int x, int y, int w, int h) {
     m[1][1] = h/2.f;
     m[2][2] = depth/2.f;
     return m;
+}
+
+Matrix lookat(Vec3f eye, Vec3f center, Vec3f up) {
+    Vec3f z = (eye-center).normalize();
+    Vec3f x = (up^z).normalize();
+    Vec3f y = (z^x).normalize();
+    Matrix res = Matrix::identity(4);
+    for (int i=0; i<3; i++) {
+        res[0][i] = x[i];
+        res[1][i] = y[i];
+        res[2][i] = z[i];
+        res[3][i] = center[i];
+    }
+    return res;
 }
 
 void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2, TGAImage &image, int *zbuffer) {
@@ -40,14 +56,15 @@ void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2, 
         float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
         Vec3i A    =               t0  + Vec3f(t2-t0  )*alpha;
         Vec3i B    = second_half ? t1  + Vec3f(t2-t1  )*beta : t0  + Vec3f(t1-t0  )*beta;
-        float ityA =               ity0 +      (ity2-ity0)*alpha;
-        float ityB = second_half ? ity1 +      (ity2-ity1)*beta : ity0 +      (ity1-ity0)*beta;
+        float ityA =               ity0 +   (ity2-ity0)*alpha;
+        float ityB = second_half ? ity1 +   (ity2-ity1)*beta : ity0 +   (ity1-ity0)*beta;
         if (A.x>B.x) { std::swap(A, B); std::swap(ityA, ityB); }
         for (int j=A.x; j<=B.x; j++) {
             float phi = B.x==A.x ? 1. : (float)(j-A.x)/(B.x-A.x);
             Vec3i    P = Vec3f(A) +  Vec3f(B-A)*phi;
             float ityP =    ityA  + (ityB-ityA)*phi;
             int idx = P.x+P.y*width;
+            if (P.x>=width||P.y>=height||P.x<0||P.y<0) continue;
             if (zbuffer[idx]<P.z) {
                 zbuffer[idx] = P.z;
                 image.set(P.x, P.y, TGAColor(255, 255, 255)*ityP);
@@ -69,9 +86,11 @@ int main(int argc, char** argv) {
     }
 
     { // draw the model
+        Matrix ModelView  = lookat(eye, center, Vec3f(0,1,0));
+        std::cerr << ModelView << std::endl;
         Matrix Projection = Matrix::identity(4);
         Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
-        Projection[3][2] = -1.f/camera.z;
+        Projection[3][2] = -1.f/(eye-center).norm();
 
         TGAImage image(width, height, TGAImage::RGB);
         for (int i=0; i<model->nfaces(); i++) {
@@ -81,7 +100,7 @@ int main(int argc, char** argv) {
             float intensity[3];
             for (int j=0; j<3; j++) {
                 Vec3f v = model->vert(face[j]);
-                screen_coords[j] =  Vec3f(ViewPort*Projection*Matrix(v));
+                screen_coords[j] =  Vec3f(ViewPort*Projection*ModelView*Matrix(v));
                 world_coords[j]  = v;
                 intensity[j] = model->norm(i, j)*light_dir;
             }
