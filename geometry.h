a23d22459fd31e67aa72a070ff28a44427d5dbd8
ssloy <spam@skisa.org>
2015-01-26 20:35:34 +0100

    still refactoring

--- geometry.h
+++ geometry.h
@@ -10,41 +10,8 @@ template<size_t DimCols,size_t DimRows,typename T> class mat;
 
 template <size_t DIM, typename T> struct vec {
     vec() { for (size_t i=DIM; i--; data_[i] = T(0)); }
-
           T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
     const T& operator[](const size_t i) const { assert(i<DIM); return data_[i]; }
-
-    vec<DIM,T> operator+(const vec<DIM,T>& v) {
-        vec<DIM, T> ret(*this);
-        for (size_t i=DIM; i--; ret[i]+=v[i]);
-        return ret;
-    }
-
-    vec<DIM,T> operator-(const vec<DIM,T>& v) {
-        vec<DIM, T> ret(*this);
-        for (size_t i=DIM; i--; ret[i]-=v[i]);
-        return ret;
-    }
-
-    T operator*(const vec<DIM,T>& v) const {
-        T ret = T();
-        for (size_t i=DIM; i--; ret += (*this)[i]*v[i]);
-        return ret;
-    }
-
-    vec<DIM,T> operator *(T u) {
-        vec<DIM, T> ret(*this);
-        for (size_t i=DIM; i--; ret[i]*=u);
-        return ret;
-    }
-
-/*
-    template <typename U> vec<DIM,T> operator *(U u) {
-        vec<DIM, T> ret(*this);
-        for (size_t i=DIM; i--; ret[i]*=u);
-        return ret;
-    }
-*/
 private:
     T data_[DIM];
 };
@@ -55,14 +22,8 @@ template <typename T> struct vec<2,T> {
     vec() : x(0), y(0) {}
     vec(T X, T Y) : x(X), y(Y) {}
     template <class U> vec<2,T>(const vec<2,U> &v);
-
           T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
     const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
-    vec<2,T> operator +(const vec<2,T> &v) { return vec<2,T>(x+v.x, y+v.y); }
-    vec<2,T> operator -(const vec<2,T> &v) { return vec<2,T>(x-v.x, y-v.y); }
-    T        operator *(const vec<2,T> &v) { return x*v.x + y*v.y; }
-    vec<2,T> operator *(T u) { return vec<2,T>(x*u, y*u); }
-//    template <typename U> vec<2,T> operator *(U u) { return vec<2,T>(x*u, y*u); }
 
     T x,y;
 };
@@ -73,15 +34,8 @@ template <typename T> struct vec<3,T> {
     vec() : x(0), y(0), z(0) {}
     vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
     template <class U> vec<3,T>(const vec<3,U> &v);
-
           T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
     const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
-    vec<3,T> operator +(const vec<3,T> &v) { return vec<3,T>(x+v.x, y+v.y, z+v.z); }
-    vec<3,T> operator -(const vec<3,T> &v) { return vec<3,T>(x-v.x, y-v.y, z-v.z); }
-    T        operator *(const vec<3,T> &v) { return x*v.x + y*v.y + z*v.z; }
-    vec<3,T> operator *(T u) { return vec<3,T>(x*u, y*u, z*u); }
-//    template <typename U> vec<3,T> operator *(U u) { return vec<3,T>(x*u, y*u, z*u); }
-
     float norm() { return std::sqrt(x*x+y*y+z*z); }
     vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
 
@@ -90,30 +44,40 @@ template <typename T> struct vec<3,T> {
 
 /////////////////////////////////////////////////////////////////////////////////
 
-template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, vec<DIM,T>& v) {
-    for(unsigned int i=0; i<DIM; i++) {
-        out << v[i] << " " ;
-    }
-    return out ;
+template<size_t DIM,typename T> T operator*(const vec<DIM,T>& lhs, const vec<DIM,T>& rhs) {
+    T ret = T();
+    for (size_t i=DIM; i--; ret+=lhs[i]*rhs[i]);
+    return ret;
 }
 
-template <class T> std::ostream& operator<<(std::ostream& out, vec<3,T>& v) {
-    out << v.x << " " << v.y << " " << v.z;
-    return out ;
+
+template<size_t DIM,typename T>vec<DIM,T> operator+(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+    for (size_t i=DIM; i--; lhs[i]+=rhs[i]);
+    return lhs;
 }
 
-template <class T> std::ostream& operator<<(std::ostream& out, vec<2,T>& v) {
-    out << v.x << " " << v.y;
-    return out ;
+template<size_t DIM,typename T>vec<DIM,T> operator-(vec<DIM,T> lhs, const vec<DIM,T>& rhs) {
+    for (size_t i=DIM; i--; lhs[i]-=rhs[i]);
+    return lhs;
 }
 
-template<size_t LEN,size_t DIM,typename T> vec<LEN,T> embed(const vec<DIM,T> &v, T fill=1) { // погружение вектора
+template<size_t DIM,typename T,typename U> vec<DIM,T> operator*(vec<DIM,T> lhs, const U& rhs) {
+    for (size_t i=DIM; i--; lhs[i]*=rhs);
+    return lhs;
+}
+
+template<size_t DIM,typename T,typename U> vec<DIM,T> operator/(vec<DIM,T> lhs, const U& rhs) {
+    for (size_t i=DIM; i--; lhs[i]*=rhs);
+    return lhs;
+}
+
+template<size_t LEN,size_t DIM,typename T> vec<LEN,T> embed(const vec<DIM,T> &v, T fill=1) {
     vec<LEN,T> ret;
     for (size_t i=LEN; i--; ret[i]=(i<DIM?v[i]:fill));
     return ret;
 }
 
-template<size_t LEN,size_t DIM, typename T> vec<LEN,T> proj(const vec<DIM,T> &v) { // погружение вектора
+template<size_t LEN,size_t DIM, typename T> vec<LEN,T> proj(const vec<DIM,T> &v) {
     vec<LEN,T> ret;
     for (size_t i=LEN; i--; ret[i]=v[i]);
     return ret;
@@ -121,6 +85,13 @@ template<size_t LEN,size_t DIM, typename T> vec<LEN,T> proj(const vec<DIM,T> &v)
 
 template <typename T> vec<3,T> cross(vec<3,T> v1, vec<3,T> v2) {
     return vec<3,T>(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+}
+
+template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, vec<DIM,T>& v) {
+    for(unsigned int i=0; i<DIM; i++) {
+        out << v[i] << " " ;
+    }
+    return out ;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -209,13 +180,13 @@ template<size_t DimRows,size_t DimCols,typename T> vec<DimRows,T> operator*(cons
 
 
 template<size_t DimRows,size_t DimCols,typename T>mat<DimCols,DimRows,T> operator/(mat<DimRows,DimCols,T> lhs, const T& rhs) {
-    for (size_t i=DimRows; i--; lhs[i]=lhs[i]*(1./rhs));
+    for (size_t i=DimRows; i--; lhs[i]=lhs[i]/rhs);
     return lhs;
 }
 
 
 template <size_t DimRows,size_t DimCols,class T> std::ostream& operator<<(std::ostream& out, mat<DimRows,DimCols,T>& m) {
-    for (size_t i=DimRows; i--; ) out << m[i] << std::endl;
+    for (size_t i=0; i<DimRows; i++) out << m[i] << std::endl;
     return out;
 }
 
