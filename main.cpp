d7e55e26d5e7cc4e2374a531a9594880f6035be9
ssloy <spam@skisa.org>
2015-02-01 21:19:52 +0100

    tangent space normal maps in fragment shader

--- main.cpp
+++ main.cpp
@@ -18,19 +18,39 @@ Vec3f        up(0,1,0);
 
 struct Shader : public IShader {
     mat<2,3,float> varying_uv;  // triangle uv coordinates, written by the vertex shader, read by the fragment shader
-    mat<4,3,float> varying_tri; // triangle coordinates (screen space), written by VS, read by FS
-
+    mat<4,3,float> varying_tri; // triangle coordinates (clip coordinates), written by VS, read by FS
+    mat<3,3,float> varying_nrm; // normal per vertex to be interpolated by FS
 
     virtual Vec4f vertex(int iface, int nthvert) {
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        varying_nrm.set_col(nthvert, proj<3>((Projection*ModelView).invert_transpose()*embed<4>(model->normal(iface, nthvert), 0.f)));
         Vec4f gl_Vertex = Projection*ModelView*embed<4>(model->vert(iface, nthvert));
         varying_tri.set_col(nthvert, gl_Vertex);
         return gl_Vertex;
     }
 
     virtual bool fragment(Vec3f bar, TGAColor &color) {
+        Vec3f bn = (varying_nrm*bar).normalize();
         Vec2f uv = varying_uv*bar;
-        color= model->diffuse(uv);
+        mat<3,3,float> ndc_tri; // column-vectors
+        for (int i=0; i<3; i++) ndc_tri.set_col(i, proj<3>(varying_tri.col(i)/varying_tri.col(i)[3]));
+        mat<3,3,float> A;
+        A[0] = ndc_tri.col(1)-ndc_tri.col(0);
+        A[1] = ndc_tri.col(2)-ndc_tri.col(0);
+        A[2] = bn;
+        A = A.invert();
+        Vec3f bu = A*Vec3f(varying_uv[0][1]-varying_uv[0][0], varying_uv[0][2]-varying_uv[0][0], 0);
+        Vec3f bv = A*Vec3f(varying_uv[1][1]-varying_uv[1][0], varying_uv[1][2]-varying_uv[1][0], 0);
+        mat<3,3,float> B;
+        B.set_col(0, bu.normalize());
+        B.set_col(1, bv.normalize());
+        B.set_col(2, bn);
+
+        Vec3f n = (B*model->normal(uv)).normalize();
+
+        float diff = std::max(0.f, n*light_dir);
+//        color = model->diffuse(uv)*diff;
+        color = TGAColor(255, 255, 255)*diff;
         return false;
     }
 };
