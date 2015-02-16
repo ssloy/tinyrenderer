631d27929c277f08c8273e94677b87768df1dc07
ssloy <spam@skisa.org>
2015-02-16 22:28:49 +0100

    ambient occlusion

--- main.cpp
+++ main.cpp
@@ -12,12 +12,14 @@ float *shadowbuffer = NULL;
 
 const int width  = 800;
 const int height = 800;
-int occlusion[1024*1024];
 
 Vec3f light_dir(1,1,1);
 Vec3f       eye(0,0,3);
 Vec3f    center(0,0,0);
 Vec3f        up(0,1,0);
+
+TGAImage total(1024, 1024, TGAImage::GRAYSCALE);
+TGAImage  occl(1024, 1024, TGAImage::GRAYSCALE);
 
 
 struct ZShader : public IShader {
@@ -49,9 +51,8 @@ struct Shader : public IShader {
     virtual bool fragment(Vec3f gl_FragCoord, Vec3f bar, TGAColor &color) {
         Vec2f uv = varying_uv*bar;
         if (std::abs(shadowbuffer[int(gl_FragCoord.x+gl_FragCoord.y*width)]-gl_FragCoord.z<1e-2)) {
-            occlusion[int(uv.x*1024) + int(uv.y*1024)*1024]++;
+            occl.set(uv.x*1024, uv.y*1024, TGAColor(255));
         }
-
         color = TGAColor(255, 0, 0);
         return false;
     }
@@ -71,22 +72,23 @@ int main(int argc, char** argv) {
         return 1;
     }
     float *zbuffer = new float[width*height];
-    for (int i=1024*1024; i--; occlusion[i] = 0);
+    shadowbuffer   = new float[width*height];
     model = new Model(argv[1]);
 
-    const int nrenders = 1;
-    for (int iter=0; iter<nrenders; iter++) {
+    const int nrenders = 1000;
+    for (int iter=1; iter<=nrenders; iter++) {
         std::cerr << iter << " from " << nrenders << std::endl;
-        std::cout <<"v " << eye << std::endl;
         for (int i=0; i<3; i++) up[i] = (float)rand()/(float)RAND_MAX;
+        eye = rand_point_on_unit_sphere();
+        eye.y = std::abs(eye.y);
+        std::cout << "v " << eye << std::endl;
 
-        shadowbuffer   = new float[width*height];
         for (int i=width*height; i--; shadowbuffer[i] = zbuffer[i] = -std::numeric_limits<float>::max());
 
         TGAImage frame(width, height, TGAImage::RGB);
         lookat(eye, center, up);
         viewport(width/8, height/8, width*3/4, height*3/4);
-        projection(-1.f/(eye-center).norm());
+        projection(0);//-1.f/(eye-center).norm());
         light_dir = proj<3>((Projection*ModelView*embed<4>(light_dir, 0.f))).normalize();
 
         ZShader zshader;
@@ -97,24 +99,26 @@ int main(int argc, char** argv) {
             triangle(zshader.varying_tri, zshader, frame, shadowbuffer);
         }
         Shader shader;
+        occl.clear();
         for (int i=0; i<model->nfaces(); i++) {
             for (int j=0; j<3; j++) {
                 shader.vertex(i, j);
             }
             triangle(shader.varying_tri, shader, frame, zbuffer);
         }
-        frame.flip_vertically(); // to place the origin in the bottom left corner of the image
-        frame.write_tga_file("framebuffer.tga");
-    }
-    TGAImage ocim(1024, 1024, TGAImage::GRAYSCALE);
-    for (int i=0; i<1024; i++) {
-        for (int j=0; j<1024; j++) {
-            ocim.set(i, j, TGAColor(255.*occlusion[i+j*1024]/float(nrenders)));
+
+//        occl.gaussian_blur(5);
+        for (int i=0; i<1024; i++) {
+            for (int j=0; j<1024; j++) {
+                float tmp = total.get(i,j)[0];
+                total.set(i, j, TGAColor((tmp*(iter-1)+occl.get(i,j)[0])/(float)iter+.5f));
+            }
         }
     }
-//    ocim.gaussian_blur(5);
-    ocim.flip_vertically();
-    ocim.write_tga_file("occlusion.tga");
+    total.flip_vertically();
+    total.write_tga_file("occlusion.tga");
+    occl.flip_vertically();
+    occl.write_tga_file("occl.tga");
 
     delete [] zbuffer;
     delete model;
