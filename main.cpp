4f619ee66d392005dd864b915744effab465130f
ssloy <dmitry.sokolov@univ-lorraine.fr>
2016-03-13 14:49:57 +0100

    preparation for tangent space computation lesson

--- main.cpp
+++ main.cpp
@@ -32,25 +32,9 @@ struct Shader : public IShader {
     virtual bool fragment(Vec3f bar, TGAColor &color) {
         Vec3f bn = (varying_nrm*bar).normalize();
         Vec2f uv = varying_uv*bar;
-        mat<3,3,float> ndc_tri; // column-vectors
-        for (int i=0; i<3; i++) ndc_tri.set_col(i, proj<3>(varying_tri.col(i)/varying_tri.col(i)[3]));
-        mat<3,3,float> A;
-        A[0] = ndc_tri.col(1)-ndc_tri.col(0);
-        A[1] = ndc_tri.col(2)-ndc_tri.col(0);
-        A[2] = bn;
-        A = A.invert();
-        Vec3f bu = A*Vec3f(varying_uv[0][1]-varying_uv[0][0], varying_uv[0][2]-varying_uv[0][0], 0);
-        Vec3f bv = A*Vec3f(varying_uv[1][1]-varying_uv[1][0], varying_uv[1][2]-varying_uv[1][0], 0);
-        mat<3,3,float> B;
-        B.set_col(0, bu.normalize());
-        B.set_col(1, bv.normalize());
-        B.set_col(2, bn);
 
-        Vec3f n = (B*model->normal(uv)).normalize();
-
-        float diff = std::max(0.f, n*light_dir);
-//        color = model->diffuse(uv)*diff;
-        color = TGAColor(255, 255, 255)*diff;
+        float diff = std::max(0.f, bn*light_dir);
+        color = model->diffuse(uv)*diff;
         return false;
     }
 };
