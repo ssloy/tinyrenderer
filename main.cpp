1d65cbf15b467cdaff185de5e13309d80259a6c9
Dmitry V. Sokolov <ha@haqr.eu>
2025-02-22 08:19:26 +0100

    Bresenham #1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    for (float t=0.; t<1.; t+=.02) {
+        int x = std::round( ax + (bx-ax)*t );
+        int y = std::round( ay + (by-ay)*t );
+        framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -15,6 +23,11 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
