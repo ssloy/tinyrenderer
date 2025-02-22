3998cbe5a5d31c7d3ab188ae5c634370644bf977
Dmitry V. Sokolov <ha@haqr.eu>
2025-02-22 08:31:23 +0100

    Bresenham #3

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    bool steep = std::abs(ax-bx) < std::abs(ay-by);
+    if (steep) { // if the line is steep, we transpose the image
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
     if (ax>bx) { // make it left−to−right
         std::swap(ax, bx);
         std::swap(ay, by);
@@ -15,7 +20,10 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     for (int x=ax; x<=bx; x++) {
         float t = (x-ax) / static_cast<float>(bx-ax);
         int y = std::round( ay + (by-ay)*t );
-        framebuffer.set(x, y, color);
+        if (steep) // if transposed, de−transpose
+            framebuffer.set(y, x, color);
+        else
+            framebuffer.set(x, y, color);
     }
 }
 
