9a6176d3b79d6bb90da0e90f5671697f32ddadc3
Dmitry Sokolov <dmitry.sokolov@univ-lorraine.fr>
2025-04-22 13:19:15 +0200

    central projection

--- main.cpp
+++ main.cpp
@@ -40,6 +40,11 @@ vec3 rot(vec3 v) {
     return Ry*v;
 }
 
+vec3 persp(vec3 v) {
+    constexpr double c = 3.;
+    return v / (1-v.z/c);
+}
+
 std::tuple<int,int,int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
     return { (v.x + 1.) *  width/2,       // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
              (v.y + 1.) * height/2,       // we want to shift the vector (x,y) and then scale it to span the entire screen.
@@ -57,9 +62,9 @@ int main(int argc, char** argv) {
     TGAImage     zbuffer(width, height, TGAImage::GRAYSCALE);
 
     for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
-        auto [ax, ay, az] = project(rot(model.vert(i, 0)));
-        auto [bx, by, bz] = project(rot(model.vert(i, 1)));
-        auto [cx, cy, cz] = project(rot(model.vert(i, 2)));
+        auto [ax, ay, az] = project(persp(rot(model.vert(i, 0))));
+        auto [bx, by, bz] = project(persp(rot(model.vert(i, 1))));
+        auto [cx, cy, cz] = project(persp(rot(model.vert(i, 2))));
         TGAColor rnd;
         for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
         triangle(ax, ay, az, bx, by, bz, cx, cy, cz, zbuffer, framebuffer, rnd);
