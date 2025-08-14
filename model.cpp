f68629a190055e702493bcfa7d7f91427b2be722
Dmitry V. Sokolov <ha@haqr.eu>
2025-08-14 16:25:41 +0200

    global space normal mapping

--- model.cpp
+++ model.cpp
@@ -13,19 +13,25 @@ Model::Model(const std::string filename) {
         char trash;
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
-            vec3 v;
+            vec4 v = {0,0,0,1};
             for (int i : {0,1,2}) iss >> v[i];
             verts.push_back(v);
         } else if (!line.compare(0, 3, "vn ")) {
             iss >> trash >> trash;
-            vec3 n;
+            vec4 n;
             for (int i : {0,1,2}) iss >> n[i];
             norms.push_back(normalized(n));
+        } else if (!line.compare(0, 3, "vt ")) {
+            iss >> trash >> trash;
+            vec2 uv;
+            for (int i : {0,1}) iss >> uv[i];
+            tex.push_back({uv.x, 1-uv.y});
         } else if (!line.compare(0, 2, "f ")) {
             int f,t,n, cnt = 0;
             iss >> trash;
             while (iss >> f >> trash >> t >> trash >> n) {
                 facet_vrt.push_back(--f);
+                facet_tex.push_back(--t);
                 facet_nrm.push_back(--n);
                 cnt++;
             }
@@ -36,20 +42,36 @@ Model::Model(const std::string filename) {
         }
     }
     std::cerr << "# v# " << nverts() << " f# "  << nfaces() << std::endl;
+    auto load_texture = [&filename](const std::string suffix, TGAImage &img) {
+        size_t dot = filename.find_last_of(".");
+        if (dot==std::string::npos) return;
+        std::string texfile = filename.substr(0,dot) + suffix;
+        std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+    };
+    load_texture("_nm.tga", normalmap);
 }
 
 int Model::nverts() const { return verts.size(); }
 int Model::nfaces() const { return facet_vrt.size()/3; }
 
-vec3 Model::vert(const int i) const {
+vec4 Model::vert(const int i) const {
     return verts[i];
 }
 
-vec3 Model::vert(const int iface, const int nthvert) const {
+vec4 Model::vert(const int iface, const int nthvert) const {
     return verts[facet_vrt[iface*3+nthvert]];
 }
 
-vec3 Model::normal(const int iface, const int nthvert) const {
+vec4 Model::normal(const int iface, const int nthvert) const {
     return norms[facet_nrm[iface*3+nthvert]];
+}
+
+vec4 Model::normal(const vec2 &uv) const {
+    TGAColor c = normalmap.get(uv[0]*normalmap.width(), uv[1]*normalmap.height());
+    return vec4{(double)c[2],(double)c[1],(double)c[0],0}*2./255. - vec4{1,1,1,0};
+}
+
+vec2 Model::uv(const int iface, const int nthvert) const {
+    return tex[facet_tex[iface*3+nthvert]];
 }
 
