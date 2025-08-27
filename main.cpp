bb0d879e81413bead25584b542e3e4bd01b35e81
Dmitry V. Sokolov <ha@haqr.eu>
2025-08-27 22:04:53 +0200

    ambient occlusion

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <random>
+#include <cmath>
+
 #include "our_gl.h"
 #include "model.h"
 
@@ -30,34 +33,9 @@ int main(int argc, char** argv) {
     constexpr int height = 800;
     constexpr int shadoww = 8000;    // shadow map buffer size
     constexpr int shadowh = 8000;
-    constexpr vec3  light{ 1, 1, 1}; // light source
     constexpr vec3    eye{-1, 0, 2}; // camera position
     constexpr vec3 center{ 0, 0, 0}; // camera direction
     constexpr vec3     up{ 0, 1, 0}; // camera up vector
-
-    std::vector<double> zshadow;
-    mat<4,4> ShadowMatrix;
-    { // shadow rendering pass
-        lookat(light, center, up);
-        init_perspective(norm(eye-center));
-        init_viewport(shadoww/16, shadowh/16, shadoww*7/8, shadowh*7/8);
-        init_zbuffer(shadoww, shadowh);
-        TGAImage framebuffer(shadoww, shadowh, TGAImage::RGB, {177, 195, 209, 255});
-
-        for (int m=1; m<argc; m++) {                    // iterate through all input objects
-            Model model(argv[m]);                       // load the data
-            EmptyShader shader{model};
-            for (int f=0; f<model.nfaces(); f++) {      // iterate through all facets
-                Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
-                                  shader.vertex(f, 1),
-                                  shader.vertex(f, 2) };
-                rasterize(clip, shader, framebuffer);   // rasterize the primitive
-            }
-        }
-        zshadow = zbuffer;
-        ShadowMatrix = Viewport * Perspective * ModelView;
-        framebuffer.write_tga_file("shadowmap.tga");
-    }
 
     // usual rendering pass
     lookat(eye, center, up);
@@ -78,18 +56,69 @@ int main(int argc, char** argv) {
     }
     framebuffer.write_tga_file("framebuffer.tga");
 
-    // post-processing
+    std::vector<double> mask(width*height, 0);
+    std::vector<double> zbuffer_copy = zbuffer;
+    mat<4,4> M = (Viewport * Perspective * ModelView).invert();
+
+    // shadow rendering pass
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+    constexpr int n = 1000;
+
+    for (int i = 0; i < n; ++i) {
+        std::cerr << i << std::endl;
+        double y = dist(gen);
+        double theta = 2.0 * M_PI * dist(gen);
+        double r = std::sqrt(1.0 - y*y);
+        vec3 light = vec3{r * std::cos(theta), y, r * std::sin(theta)}*1.5;
+
+        std::cout << "v " << light << std::endl;
+
+        lookat(light, center, up);
+        ModelView[3][3] =    norm(light-center);
+
+        init_perspective(norm(eye-center));
+        init_viewport(shadoww/16, shadowh/16, shadoww*7/8, shadowh*7/8);
+        init_zbuffer(shadoww, shadowh);
+        TGAImage trash(shadoww, shadowh, TGAImage::RGB, {177, 195, 209, 255});
+
+        for (int m=1; m<argc; m++) {                    // iterate through all input objects
+            Model model(argv[m]);                       // load the data
+            EmptyShader shader{model};
+            for (int f=0; f<model.nfaces(); f++) {      // iterate through all facets
+                Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
+                    shader.vertex(f, 1),
+                    shader.vertex(f, 2) };
+                rasterize(clip, shader, trash);         // rasterize the primitive
+            }
+        }
+//      trash.write_tga_file(std::string("shadowmap") + std::to_string(i) + std::string(".tga"));
+
+        mat<4,4> N = Viewport * Perspective * ModelView;
+
+        // post-processing
+#pragma omp parallel for
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++) {
+                vec4 fragment = M * vec4{x, y, zbuffer_copy[x+y*width], 1.};
+                vec4 q = N * fragment;
+                vec3 p = q.xyz()/q.w;
+                double lit =  (fragment.z<-100 ||                                     // it's the background or
+                               (p.x>=0 && p.x<shadoww && p.y>=0 && p.y<shadowh &&     // it is not out of bounds of the shadow buffer
+                               (p.z > zbuffer[int(p.x) + int(p.y)*shadoww] - .03)));  // it is visible
+                mask[x+y*width] += (lit - mask[x+y*width])/(i+1.);
+            }
+        }
+
+    }
+
+
 #pragma omp parallel for
     for (int x=0; x<width; x++) {
         for (int y=0; y<height; y++) {
-            vec4 fragment = (Viewport * Perspective * ModelView).invert() * vec4{x, y, zbuffer[x+y*width], 1.};
-            vec4 q = ShadowMatrix * fragment;
-            vec3 p = q.xyz()/q.w;
-            if (p.x<0 || p.x>=shadoww || p.y<0 || p.y>=shadowh) continue;
-            if (p.z < zshadow[int(p.x) + int(p.y)*shadoww] - .03) { // small bias to avoid z-fighting
-                TGAColor c = framebuffer.get(x, y);
-                framebuffer.set(x, y, { c[0]/2, c[1]/2, c[2]/2, c[3] });
-            }
+            double m = std::min(1., 1.3*mask[x+y*width]);
+            TGAColor c = framebuffer.get(x, y);
+            framebuffer.set(x, y, { c[0]*m, c[1]*m, c[2]*m, c[3] });
         }
     }
     framebuffer.write_tga_file("shadow.tga");
@@ -104,8 +133,8 @@ int main(int argc, char** argv) {
                     constexpr int Gx[3][3] = { {-1,  0,  1}, {-2, 0, 2}, {-1, 0, 1} };
                     constexpr int Gy[3][3] = { {-1, -2, -1}, { 0, 0, 0}, { 1, 2, 1} };
                     sum = sum + vec2{
-                        Gx[j + 1][i + 1] * zbuffer[x+i + (y+j)*width],
-                        Gy[j + 1][i + 1] * zbuffer[x+i + (y+j)*width]
+                        Gx[j + 1][i + 1] * zbuffer_copy[x+i + (y+j)*width],
+                        Gy[j + 1][i + 1] * zbuffer_copy[x+i + (y+j)*width]
                     };
                 }
             }
