0b2359c86f5bac90c8c7a9aa4bfb768f692e88ee
Dmitry V. Sokolov <ha@haqr.eu>
2025-08-30 08:33:29 +0200

    toon shading

--- main.cpp
+++ main.cpp
@@ -8,18 +8,36 @@
 extern mat<4,4> Viewport, ModelView, Perspective; // "OpenGL" state matrices and
 extern std::vector<double> zbuffer;     // the depth buffer
 
-struct BlankShader : IShader {
+struct ToonShader : IShader {
+    vec4 color;
     const Model &model;
+    vec4 l;              // light direction in eye coordinates
+    vec4 varying_nrm[3]; // normal per vertex to be interpolated by the fragment shader
 
-    BlankShader(const Model &m) : model(m) {}
+    ToonShader(const vec4 color, const vec3 light, const Model &m) : color(color), model(m) {
+        l = normalized((ModelView*vec4{light.x, light.y, light.z, 0.})); // transform the light vector to view coordinates
+    }
 
     virtual vec4 vertex(const int face, const int vert) {
+        varying_nrm[vert] = ModelView.invert_transpose() * model.normal(face, vert);
         vec4 gl_Position = ModelView * model.vert(face, vert);
         return Perspective * gl_Position;
     }
 
     virtual std::pair<bool,TGAColor> fragment(const vec3 bar) const {
-        return {false, {255, 255, 255, 255}};
+        vec4 n = normalized(varying_nrm[0] * bar[0] + varying_nrm[1] * bar[1] + varying_nrm[2] * bar[2]); // per-vertex normal interpolation
+
+        double diffuse = std::max(0., n * l); // diffuse light intensity
+
+        double intensity = .15 + diffuse;   // a bit of ambient light + diffuse light
+        if (intensity>.66) intensity = 1;
+        else if (intensity>.33) intensity = .66;
+        else intensity = .33;
+
+        TGAColor gl_FragColor;
+        for (int channel : {0,1,2})
+            gl_FragColor[channel] = std::min<int>(255, color[channel]*intensity);
+        return {false, gl_FragColor};                             // do not discard the pixel
     }
 };
 
@@ -31,6 +49,7 @@ int main(int argc, char** argv) {
 
     constexpr int width  = 800;      // output image size
     constexpr int height = 800;
+    constexpr vec3  light{ 1, 1, 1}; // light source
     constexpr vec3    eye{-1, 0, 2}; // camera position
     constexpr vec3 center{ 0, 0, 0}; // camera direction
     constexpr vec3     up{ 0, 1, 0}; // camera up vector
@@ -42,9 +61,11 @@ int main(int argc, char** argv) {
     init_zbuffer(width, height);
     TGAImage framebuffer(width, height, TGAImage::RGB, {177, 195, 209, 255});
 
+    constexpr vec4 colors[] = {{22*4, 56*4, 147*4, 255}, {123, 98, 88, 255}};
+
     for (int m=1; m<argc; m++) {                    // iterate through all input objects
         Model model(argv[m]);                       // load the data
-        BlankShader shader{model};
+        ToonShader shader(colors[(m-1)%2], light, model);
         for (int f=0; f<model.nfaces(); f++) {      // iterate through all facets
             Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
                               shader.vertex(f, 1),
@@ -53,35 +74,23 @@ int main(int argc, char** argv) {
         }
     }
 
-    constexpr double ao_radius = .1;  // ssao ball radius in normalized device coordinates
-    constexpr int nsamples = 128;     // number of samples in the ball
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dist(-ao_radius, ao_radius);
-    auto smoothstep = [](double edge0, double edge1, double x) {         // smoothstep returns 0 if the input is less than the left edge,
-            double t = std::clamp((x - edge0)/(edge1 - edge0), 0., 1.);  // 1 if the input is greater than the right edge,
-            return t*t*(3 - 2*t);                                        // Hermite interpolation inbetween. The derivative of the smoothstep function is zero at both edges.
-    };
-
-#pragma omp parallel for
-    for (int x=0; x<width; x++) {
-        for (int y=0; y<height; y++) {
-            double z = zbuffer[x+y*width];
-            if (z<-100) continue;
-            vec4 fragment = Viewport.invert() * vec4{x, y, z, 1.}; // for each fragment in the framebuffer
-            double vote   = 0;
-            double voters = 0;
-            for(int i=0; i<nsamples; i++) {                                         // compute a very rough approximation of the solid angle
-                vec4 p = Viewport * (fragment + vec4{dist(gen), dist(gen), dist(gen), 0.});
-                if (p.x<0 || p.x>=width || p.y<0 || p.y>=height) continue;
-                double d = zbuffer[int(p.x) + int(p.y)*width];
-                if (z + 5*ao_radius < d) continue;                         // range check to remove the dark halo
-                voters++;
-                vote += d > p.z;
+    // post-processing: edge detection
+    constexpr double threshold = .15;
+    for (int y = 1; y < framebuffer.height() - 1; ++y) {
+        for (int x = 1; x < framebuffer.width() - 1; ++x) {
+            vec2 sum;
+            for (int j = -1; j <= 1; ++j) {
+                for (int i = -1; i <= 1; ++i) {
+                    constexpr int Gx[3][3] = { {-1,  0,  1}, {-2, 0, 2}, {-1, 0, 1} };
+                    constexpr int Gy[3][3] = { {-1, -2, -1}, { 0, 0, 0}, { 1, 2, 1} };
+                    sum = sum + vec2{
+                        Gx[j + 1][i + 1] * zbuffer[x+i + (y+j)*width],
+                        Gy[j + 1][i + 1] * zbuffer[x+i + (y+j)*width]
+                    };
+                }
             }
-            double ssao = smoothstep(0, 1, 1 - vote/voters*.4);
-            TGAColor c = framebuffer.get(x, y);
-            framebuffer.set(x, y, { c[0]*ssao, c[1]*ssao, c[2]*ssao, c[3] });
+            if (norm(sum)>threshold)
+                framebuffer.set(x, y, TGAColor{0, 0, 0, 255});
         }
     }
 
